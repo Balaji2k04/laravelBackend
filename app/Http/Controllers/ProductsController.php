56a118ed8ae644e1735659024baffa8d5c64e934
Balaji <balaji@neosme.com>
2023-10-31 11:42:06 +0530

    completed

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class EcommerceController extends Controller
+class ProductsController extends Controller
 {
     private $database;
 
